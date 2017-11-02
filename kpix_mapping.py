b79be29b4595eaaf41b108e7eaa42cc35e29a870
Kraemer Uwe <uwe.kraemer@desy.de>
2017-11-02 10:44:21 +0100

    Put ecal mapping into a function, added option to choose which type of mappin to use (ECAL; Tracker; KPiX)

--- kpix_mapping.py
+++ kpix_mapping.py
@@ -19,6 +19,81 @@ def numberlist(count,num_of_x):
     listofnums = [count] * int(num_of_x)
     return listofnums
 
+def ECAL_map(column, row, num_of_x, ymin, ymax):
+	for count in xrange(39):
+		if ( count == 0 ):
+			column.extend(np.arange(ymax, ymin, -1))
+			num_of_x = ymax-ymin
+			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+		if (( count > 0 ) and (count <= 7)):
+			ymax = ymax + 1.5
+			ymin = ymin - 1.5
+			column.extend(np.arange(ymax, ymin, -1))
+			num_of_x = ymax-ymin
+			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+		if ((count >= 8) and (count <= 11)):
+			ymax = ymax + 0.5
+			ymin = ymin - 0.5
+			column.extend(np.arange(ymax, ymin, -1))
+			num_of_x = ymax-ymin
+			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+		if ((count >= 12) and (count < 28)):
+			if (count%2 == 1):
+				ymax = ymax - 0.5
+				ymin = ymin + 0.5
+				num_of_x = ymax-ymin
+			else:
+				ymax = ymax + 0.5
+				ymin = ymin - 0.5
+				num_of_x = ymax-ymin
+			if ((count >= 15) and (count <= 23)):
+				if ((count == 15) or (count == 23)):
+					column.extend(np.arange(ymax,ymin+num_of_x/2+0.5, -1))
+					column.extend(np.arange(ymax-num_of_x/2+0.5, ymin+num_of_x/2-0.5, -0.5))
+					column.extend(np.arange(ymax-num_of_x/2-0.5,ymin, -1))
+					num_of_x = ymax-ymin + 1
+					#print num_of_x
+					row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+				elif ((count == 16) or (count == 22)):
+					column.extend(np.arange(ymax,ymin+num_of_x/2+3, -1))
+					column.extend(np.arange(ymax-num_of_x/2+3, ymin+num_of_x/2-3, -0.5))
+					column.extend(np.arange(ymax-num_of_x/2-3,ymin, -1))
+					num_of_x = ymax-ymin + 6
+					#print num_of_x
+					row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+				elif ((count == 17) or (count == 19) or (count == 21)):
+					column.extend(np.arange(ymax,ymin+num_of_x/2+3.5, -1))
+					column.extend(np.arange(ymax-num_of_x/2+3.5, ymin+num_of_x/2-3.5, -0.5))
+					column.extend(np.arange(ymax-num_of_x/2-3.5,ymin, -1))
+					num_of_x = ymax-ymin + 7
+					#print num_of_x
+					row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+				elif ((count == 18) or (count == 20)):
+					column.extend(np.arange(ymax,ymin+num_of_x/2+4, -1))
+					column.extend(np.arange(ymax-num_of_x/2+4, ymin+num_of_x/2-4, -0.5))
+					column.extend(np.arange(ymax-num_of_x/2-4,ymin, -1))
+					num_of_x = ymax-ymin + 8
+					#print num_of_x
+					row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+			else:
+				column.extend(np.arange(ymax, ymin, -1))
+				row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+		if ((count >= 28) and (count <= 31)):
+			ymax = ymax - 0.5
+			ymin = ymin + 0.5
+			num_of_x = ymax-ymin
+			column.extend(np.arange(ymax, ymin, -1))
+			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+		if (count >= 32):
+			ymax = ymax - 1.5
+			ymin = ymin + 1.5
+			num_of_x = ymax-ymin
+			column.extend(np.arange(ymax, ymin, -1))
+			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
+#Experimental!
+def tracker_map():
+	
+
 def map_text(ax, channel_kpix, mapping, row, column):
 	pixel = 0
 	for i in channel_kpix:
@@ -34,7 +109,7 @@ def map_text(ax, channel_kpix, mapping, row, column):
 	return ax
 
 
-def map_plot(channel_kpix, mapping, sensor_row, sensor_column, color, list_of_channel_values, bucket_name, bucket_nr, color_values, type_name, color_type):
+def map_plot_ecal(channel_kpix, mapping, sensor_row, sensor_column, color, list_of_channel_values, bucket_name, bucket_nr, color_values, type_name, color_type):
 		fig = plt.figure()
 		ax = fig.add_subplot(111) 
 		map_text(ax, channel_kpix, mapping, row, column)
@@ -67,6 +142,38 @@ def map_plot(channel_kpix, mapping, sensor_row, sensor_column, color, list_of_ch
 		plt.savefig(Filename, dpi = 300)
 		plt.close()
 
+def map_plot_tracker(channel_kpix, mapping, sensor_row, sensor_column, color, list_of_channel_values, bucket_name, bucket_nr, color_values, type_name, color_type):
+		fig = plt.figure()
+		ax = fig.add_subplot(111) 
+		map_text(ax, channel_kpix, mapping, row, column)
+		plt.xlim(0,1840)
+		plt.ylim(0, 1840)
+		plt.axis('off')
+		if ('log' in color_type):
+			plsctr = plt.scatter(sensor_row, sensor_column, c=list_of_channel_values[bucket_nr], s = 100, marker='_', norm=mpl.colors.SymLogNorm(linthresh = color_values[1], vmin=color_values[0], vmax=color_values[-1]))
+		elif ('log' not in color_type):
+			plsctr = plt.scatter(sensor_row, sensor_column,  c=list_of_channel_values[bucket_nr], s = 100, marker='_', vmin=color_values[0], vmax=color_values[-1])
+		if args.file_out:
+			if ('mean' in type_name):
+				Filename=args.file_out  + '_mean_' + args.kpix + '_' +  bucket_name + '_map.png'
+			elif ('rms' in type_name):
+				Filename = args.file_out +'_RMS_' +  args.kpix + '_' +  bucket_name + '_map.png'
+			elif ('slope' in type_name):
+				Filename = args.file_out +'_slope_' +  args.kpix + '_' +  bucket_name +'_map.png'
+			elif ('entry' in type_name):
+				Filename = args.file_out +'_entries_' +  args.kpix + '_' +  bucket_name + '_map.png'
+		elif ('mean' in type_name):
+			Filename = args.file_in +'_mean_' +  args.kpix + '_' +  bucket_name + '_map.png'
+		elif ('rms' in type_name):
+			Filename = args.file_in +'_RMS_' +  args.kpix + '_' +  bucket_name + '_map.png'
+		elif ('slope' in type_name):
+			Filename = args.file_in +'_slope_' +  args.kpix + '_' +  bucket_name + '_map.png'
+		elif ('entry' in type_name):
+			Filename = args.file_in +'_entries_' +  args.kpix + '_' +  bucket_name + '_map.png'
+		plt.colorbar(plsctr)
+		print "File is saved in " +  Filename
+		plt.savefig(Filename, dpi = 300)
+		plt.close()
 #def plot_3D(channel_kpix, sensor_row, sensor_column, list_of_channel_values, bucket_name, bucket_nr, type_name):
 	#fig = plt.figure()
 	#ax = fig.gca(projection='3d')
@@ -127,7 +234,7 @@ parser.add_argument('-k', '--kpix', dest='kpix', type = str, help='Choose the KP
 #parser.add_argument('-f', '--file_in', dest='file_in', help="Input filename")
 parser.add_argument('-o', '--file_out', dest='file_out', help="Output filename")
 parser.add_argument('--calib', dest='calib', help='True if calibration file')
-#parser.add_argument('-s', '--style', dest='style', help='Type of plot, 2D = 2Dwith colors, 3D = 3D')
+parser.add_argument('-s', dest='sensor', help='Chosse which sensor to map the results to. (e.g. ecal, tracker, kpix)')
 parser.add_argument('-t', '--timed', dest='timed', help='Choose whether or not to take time stamped plots')
 args = parser.parse_args()
 
@@ -254,115 +361,52 @@ for i in channel_kpix:
 #print num_of_entries_sensor[0][0]
 pixel_count = []
 x = 0
-for count in xrange(39):
-	if ( count == 0 ):
-		column.extend(np.arange(ymax, ymin, -1))
-		num_of_x = ymax-ymin
-		row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-	if (( count > 0 ) and (count <= 7)):
-		ymax = ymax + 1.5
-		ymin = ymin - 1.5
-		column.extend(np.arange(ymax, ymin, -1))
-		num_of_x = ymax-ymin
-		row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-	if ((count >= 8) and (count <= 11)):
-		ymax = ymax + 0.5
-		ymin = ymin - 0.5
-		column.extend(np.arange(ymax, ymin, -1))
-		num_of_x = ymax-ymin
-		row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-	if ((count >= 12) and (count < 28)):
-		if (count%2 == 1):
-			ymax = ymax - 0.5
-			ymin = ymin + 0.5
-			num_of_x = ymax-ymin
-		else:
-			ymax = ymax + 0.5
-			ymin = ymin - 0.5
-			num_of_x = ymax-ymin
-		if ((count >= 15) and (count <= 23)):
-			if ((count == 15) or (count == 23)):
-				column.extend(np.arange(ymax,ymin+num_of_x/2+0.5, -1))
-				column.extend(np.arange(ymax-num_of_x/2+0.5, ymin+num_of_x/2-0.5, -0.5))
-				column.extend(np.arange(ymax-num_of_x/2-0.5,ymin, -1))
-				num_of_x = ymax-ymin + 1
-				#print num_of_x
-				row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-			elif ((count == 16) or (count == 22)):
-				column.extend(np.arange(ymax,ymin+num_of_x/2+3, -1))
-				column.extend(np.arange(ymax-num_of_x/2+3, ymin+num_of_x/2-3, -0.5))
-				column.extend(np.arange(ymax-num_of_x/2-3,ymin, -1))
-				num_of_x = ymax-ymin + 6
-				#print num_of_x
-				row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-			elif ((count == 17) or (count == 19) or (count == 21)):
-				column.extend(np.arange(ymax,ymin+num_of_x/2+3.5, -1))
-				column.extend(np.arange(ymax-num_of_x/2+3.5, ymin+num_of_x/2-3.5, -0.5))
-				column.extend(np.arange(ymax-num_of_x/2-3.5,ymin, -1))
-				num_of_x = ymax-ymin + 7
-				#print num_of_x
-				row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-			elif ((count == 18) or (count == 20)):
-				column.extend(np.arange(ymax,ymin+num_of_x/2+4, -1))
-				column.extend(np.arange(ymax-num_of_x/2+4, ymin+num_of_x/2-4, -0.5))
-				column.extend(np.arange(ymax-num_of_x/2-4,ymin, -1))
-				num_of_x = ymax-ymin + 8
-				#print num_of_x
-				row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-		else:
-			column.extend(np.arange(ymax, ymin, -1))
-			row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-	if ((count >= 28) and (count <= 31)):
-		ymax = ymax - 0.5
-		ymin = ymin + 0.5
-		num_of_x = ymax-ymin
-		column.extend(np.arange(ymax, ymin, -1))
-		row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
-	if (count >= 32):
-		ymax = ymax - 1.5
-		ymin = ymin + 1.5
-		num_of_x = ymax-ymin
-		column.extend(np.arange(ymax, ymin, -1))
-		row.extend(numberlist(count*np.cos(np.pi/3), num_of_x))
 
-## BEGIN ACTUAL PLOTTING
-
-#color = [0, 150, 300, 450, 600, 750, 900]
-#bucket_nr = 0
-#for b in bucket:  #mean map
-	#map_plot(channel_kpix, mapping_sensor_to_kpix, row, column, color, adcmean_sensor, b, bucket_nr, color, 'mean', 'log')
-	#bucket_nr = bucket_nr + 1
-	
-#color_RMS = [0, 0.002, 0.004, 0.006, 0.010, 0.015, 0.020, 0.1]
-#bucket_nr = 0
-#for b in bucket: #rms map
-	#map_plot(channel_kpix, mapping_sensor_to_kpix, row, column, color_RMS, adcRMS_sensor, b, bucket_nr, color_RMS, 'rms', 'log')
-	#bucket_nr = bucket_nr + 1
-	
-color_entry = [0.0, 1E0] 
-bucket_nr = 0
-for b in bucket: #entries
-	map_plot(channel_kpix, mapping_sensor_to_kpix, row, column, color_entry, num_of_entries_sensor, b, bucket_nr, color_entry, 'entry', 'norm')
-	bucket_nr = bucket_nr + 1
-		
-#color_slope = [0, 1E15, 5E15, 1E16, 2E16]
-#bucket_nr = 0
-#print channel_slope_sensor
-#if (args.calib == True):
-#	for b in bucket: #slope map
-#		map_plot(channel_kpix, mapping_sensor_to_kpix, row, column, color_slope, channel_slope_sensor, b, bucket_nr, color_slope, 'slope', 'norm')
-#		bucket_nr = bucket_nr + 1
-			
-#elif ('3D' in args.style):	
+## PRODUCING AN EXTERNAL FUNCTION FOR MAPPING and subsequent plotting
+if ('ecal' in args.sensor):
+	ECAL_map(column, row, num_of_x, ymin, ymax)  ## PRoducing the scatter map for the ECAL sensor, row and column coordinates
+	#color = [0, 150, 300, 450, 600, 750, 900]
 	#bucket_nr = 0
-	#for b in bucket: 
-		#plot_3D(channel_kpix, row, column, adcmean_sensor, b, bucket_nr, 'mean')
+	#for b in bucket:  #mean map
+		#map_plot_ecal(channel_kpix, mapping_sensor_to_kpix, row, column, color, adcmean_sensor, b, bucket_nr, color, 'mean', 'log')
 		#bucket_nr = bucket_nr + 1
+	
+	#color_RMS = [0, 0.002, 0.004, 0.006, 0.010, 0.015, 0.020, 0.1]
 	#bucket_nr = 0
-	##for b in bucket:
-	##	plot_3D(channel_kpix, row, column, adcRMS_sensor, b, bucket_nr, 'rms')
-	##	bucket_nr = bucket_nr + 1
-	##bucket_nr = 0
-	##for b in bucket:
-	##	plot_3D(channel_kpix, row, column, channel_slope_sensor, b, bucket_nr, 'slope')
-	##	bucket_nr = bucket_nr + 1
+	#for b in bucket: #rms map
+		#map_plot_ecal(channel_kpix, mapping_sensor_to_kpix, row, column, color_RMS, adcRMS_sensor, b, bucket_nr, color_RMS, 'rms', 'log')
+		#bucket_nr = bucket_nr + 1
+	
+	color_entry = [0.0, 1E0] 
+	bucket_nr = 0
+	for b in bucket: #entries map
+		map_plot_ecal(channel_kpix, mapping_sensor_to_kpix, row, column, color_entry, num_of_entries_sensor, b, bucket_nr, color_entry, 'entry', 'norm')
+		bucket_nr = bucket_nr + 1
+		
+	#color_slope = [0, 1E15, 5E15, 1E16, 2E16]
+	#bucket_nr = 0
+	#print channel_slope_sensor
+	#if (args.calib == True):
+	#	for b in bucket: #slope map
+	#		map_plot_ecal(channel_kpix, mapping_sensor_to_kpix, row, column, color_slope, channel_slope_sensor, b, bucket_nr, color_slope, 'slope', 'norm')
+	#		bucket_nr = bucket_nr + 1
+elif ('tracker' in args.sensor):
+	#Tracker map
+	# 3680 strips over 92000 um length with 1840 being read out = 920 per KPiX
+	#_________________
+	#_________________
+	#_________________
+	#_________________
+	#_________________
+	for i in xrange(1840):
+		row.append(i)
+		column.append(920)
+	bucket_nr = 0
+	for b in bucket:
+		map_plot_tracker(channel_kpix, mapping_sensor_to_kpix, row, column, color_entry, num_of_entries_sensor, b, bucket_nr, color_entry, 'entry', 'norm')
+		bucket_nr = bucket_nr +1
+elif ('kpix' in args.sensor):
+	
+## BEGIN ACTUAL PLOTTING
+if ('ecal' in args.sensor):
+	
